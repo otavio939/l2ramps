3131727699dfc491357e0b3c8915d2cdd94014cd
guilhermeotavios <guilhermeotavios@gmail.com>
2022-11-20 19:44:41 -0500

    More styling and data

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'chatura-gray': '#A09287',
+        'fabulous-grape': '#6D344F',
+        'evergreen-fog': '#95978A',
+        'homburg-gray': '#666D69',
+        'mineral-gray': '#515763',
+        'pure-white': '#EDECE6',
+        'reddened-earth': '#9C6E63',
+        'shiitake': '#C8BCAB',
+        'silvermist': '#B0B8B2',
+        'skyline-steel': '#C6BFB3'
+      }
+    },
+    fontFamily: {
+      'body': ['Rubik', 'sans-serif'],
+      'header': ['Monoton', 'cursive']
+    }
   },
   plugins: [],
 }
