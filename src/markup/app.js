d7bec0d9a4edf12bf2e5c795177f268bce37ab62
guilhermeotavios <guilhermeotavios@gmail.com>
2022-11-21 20:41:06 -0500

    Redesigned using accordion

--- src/markup/app.js
+++ src/markup/app.js
@@ -1,3 +1,7 @@
+///////////////
+// DARK MODE
+//////////////
+
 // Icons vars
 const sunIcon = document.querySelector(".sun");
 const moonIcon = document.querySelector(".moon");
@@ -36,3 +40,35 @@ moonIcon.addEventListener("click", () => {
 
 // Invoke theme check on initial load
 themeCheck();
+
+
+///////////////
+// ACCORDION
+//////////////
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
+        };
+
+        /* Animate the accordion */
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
