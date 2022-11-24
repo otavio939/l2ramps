c4326af6ef8c93dfe7aa30377d4cb698be745d49
guilhermeotavios <guilhermeotavios@gmail.com>
2022-11-24 00:08:55 -0500

    Tables improvements and styling

--- src/markup/app.js
+++ src/markup/app.js
@@ -54,7 +54,11 @@ for (i = 0; i < acc.length; i++) {
 
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
-        this.classList.toggle("active");
+        if (document.documentElement.classList.contains("dark")) {
+            this.classList.toggle("active-dark");
+        } else {
+            this.classList.toggle("active");
+        }
 
         /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'black-magic': '#313131',
+        'carnelian': '#5B3D3D',
         'chatura-gray': '#A09287',
         'fabulous-grape': '#6D344F',
         'evergreen-fog': '#95978A',
         'homburg-gray': '#666D69',
         'mineral-gray': '#515763',
+        'mount-etna': '#3A484D',
+        'kestrel-white': '#E2D6C6',
         'pure-white': '#EDECE6',
         'reddened-earth': '#9C6E63',
         'shiitake': '#C8BCAB',
